0e6212ba5bd5e12c533805f54b660fab649bd9b5
anik-saha-2001 <aniksahamda@gmail.com>
2024-01-29 21:16:18 +0530

    fixed-img added

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
+var elemC = document.querySelector("#elem-container");
+var fixed = document.querySelector("#fixed-image");
+//on mouse enter
+elemC.addEventListener("mouseenter", function () {
+  fixed.style.display = "block";
+});
+//on mouse leave
+elemC.addEventListener("mouseleave", function () {
+  fixed.style.display = "none";
+});
+
+var elems = document.querySelectorAll(".elem");
+elems.forEach(function (e) {
+  e.addEventListener("mouseenter", function () {
+    var image = e.getAttribute("data-image");
+    fixed.style.backgroundImage = `url(${image})`;
+  });
+});
