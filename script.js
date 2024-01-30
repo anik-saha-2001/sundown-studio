c55b4b6102196fec08da10957e912511c5930a70
anik-saha-2001 <aniksahamda@gmail.com>
2024-01-30 18:37:51 +0530

    website completed

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-var elemC = document.querySelector("#elem-container");
-var fixed = document.querySelector("#fixed-image");
-//on mouse enter
-elemC.addEventListener("mouseenter", function () {
-  fixed.style.display = "block";
-});
-//on mouse leave
-elemC.addEventListener("mouseleave", function () {
-  fixed.style.display = "none";
-});
+//Locomotive Scroll
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector("#main"),
+//   smooth: true,
+// });
 
-var elems = document.querySelectorAll(".elem");
-elems.forEach(function (e) {
-  e.addEventListener("mouseenter", function () {
-    var image = e.getAttribute("data-image");
-    fixed.style.backgroundImage = `url(${image})`;
+//page4 animation
+function page4Animation() {
+  var elemC = document.querySelector("#elem-container");
+  var fixed = document.querySelector("#fixed-image");
+  //on mouse enter
+  elemC.addEventListener("mouseenter", function () {
+    fixed.style.display = "block";
   });
-});
+  //on mouse leave
+  elemC.addEventListener("mouseleave", function () {
+    fixed.style.display = "none";
+  });
+
+  var elems = document.querySelectorAll(".elem");
+  elems.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+      var image = e.getAttribute("data-image");
+      fixed.style.backgroundImage = `url(${image})`;
+    });
+  });
+}
+
+// Initialize Swiper
+function swiperAnimation() {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 100,
+  });
+}
+
+page4Animation();
+swiperAnimation();
